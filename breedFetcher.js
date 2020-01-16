1804a98af24bb9719ee18a8281af17a131d68633
Olivia <oliviapower.02@gmail.com>
2020-01-16 23:01:19 +0000

    fixing bugs

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,13 +1,14 @@
 const request = require('request');
-let args = process.argv.slice(2);
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`, function(error, message, body) {
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    throw new Error('No cat exists!!');
-  }
-  if (error) {
-    throw new Error('Request Error: ', error);
-  }
-  console.log(data[0].description);
-});
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, function(error, message, body) {
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      return callback(error, null)
+    }
+
+    return callback(error, data[0].description);
+  });
+  
+};
+module.exports = { fetchBreedDescription} ;
